5816d3aafc6a2c15bdbafce6308c707590551df5
rautry6 <94204738+rautry6@users.noreply.github.com>
2024-02-15 20:44:18 -0500

    Chiba Fix

--- src/assets/img/Projects/Chiba/index.js
+++ src/assets/img/Projects/Chiba/index.js
@@ -3,7 +3,7 @@ import ChibaTutorial from "./chibatutorial.png"
 import ChibaLevel1 from "./level1.png"
 import ChibaLevel2 from "./level2.png"
 import ChibaLevel3 from "./level3.png"
-import ChibaLevel4 from "./level4.png"
+import ChibaLevel4 from "./level4.jpg"
 import ChibaComplete from "./levelcomplete.png"
 import ChibaLevelSelect from "./levelselect.png"
 
